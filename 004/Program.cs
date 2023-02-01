a9d0288c5c3c1c6101b14026751b1afc9fe5901c
Rustam <rustamzakirov@mail.ru>
2023-02-01 18:21:15 +0300

    finish additional

---
+++ 004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите несколько чисел");
+int a = Convert.ToInt32(Console.ReadLine());
+int max = 1;
+int max2 = 0;
+while (a != 0)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > max)
+    {
+        max2 = max;
+        max = a;
+    }
+    else if (a > max2)
+        max2 = a; 
+}
+Console.WriteLine($"второе максимальное число = {max2}");
