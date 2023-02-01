684dea1c302f42384df576c424625c135cac74ec
Rustam <rustamzakirov@mail.ru>
2023-02-01 17:32:30 +0300

    third complete

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.WriteLine("введите дату");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 6 == 0 || a % 7 == 0)
+    Console.WriteLine("выходной");
+else
+    Console.WriteLine("будний день");
