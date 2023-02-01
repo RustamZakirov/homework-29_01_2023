7c7b0c67a2a352b678ed00f8f6aa11d2d882ace3
Rustam <rustamzakirov@mail.ru>
2023-02-01 16:48:53 +0300

    second complete

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,9 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+Console.WriteLine("введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 100 % 10;
+if (b < 1)
+    Console.WriteLine("третья цияра отсутствует");
+else
+    Console.WriteLine($"третье число = {b}");
